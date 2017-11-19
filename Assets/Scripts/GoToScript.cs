9e3a9f95fadf32626c9e198a0e119af536662697
Austin Ferguson <ajartcga@gmail.com>
2017-11-18 20:53:06 -0500

    Pushing Nick's pathing to multiple points

--- Assets/Scripts/GoToScript.cs
+++ Assets/Scripts/GoToScript.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class GoToScript : MonoBehaviour {
 
-    public Transform target;
+    public List<Transform> targets;
     UnityEngine.AI.NavMeshAgent agent;
+    public int wait;
+    int counter;//
+    int targetNum;
 
 	// Use this for initialization
 	void Start () {
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        agent.SetDestination(target.position);
+        counter = (int)(Random.Range(wait - 350, wait + 50));
+        targetNum = (int)Random.Range(0, targets.Count);
+        agent.SetDestination(targets[targetNum].position);
+        agent.speed = 4;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        counter--;
+        if (counter <= 0)
+        {
+            int tmp = (int)Random.Range(0, targets.Count);
+            if (tmp != targetNum) {
+                targetNum = tmp;
+                agent.SetDestination(targets[targetNum].position);
+                counter = (int)(Random.Range(wait - 50, wait + 50));
+                if (Random.Range(0, 1) < 0.5)
+                    agent.speed = 4;
+                else
+                    agent.speed = 8;
+            }
+        }
 	}
 }
